053fe3ceafe769c463d54f34a10e4eb47b87ff4e
Girard Media <429016+girardmedia@users.noreply.github.com>
2020-01-19 10:35:51 -0500

    Update jmigrate-fix.php

--- jmigrate-fix.php
+++ jmigrate-fix.php
@@ -1,23 +1,23 @@
 <?php
 /**
- * Dequeue jQuery Migrate
+ * jMigrate Fix
  *
- * @package   DequeueJqueryMigrate
- * @author    Brady Vercher
- * @link      http://www.cedaro.com/
- * @copyright Copyright (c) 2015 Cedaro, LLC
+ * @package   JMigrateFix
+ * @author    Sevak Girard
+ * @link      https://girardmedia.com/
+ * @copyright Copyright (c) 2020 Girard Media LLC
  * @license   GPL-2.0+
  *
  * @wordpress-plugin
- * Plugin Name:       Dequeue jQuery Migrate
- * Plugin URI:        https://github.com/cedaro/dequeue-jquery-migrate
+ * Plugin Name:       jMigrate Fix
+ * Plugin URI:        https://github.com/girardmedia/jmigrate-fix
  * Description:       Remove the jquery-migrate.js script dependency.
  * Version:           1.0.0
- * Author:            Cedaro
- * Author URI:        http://www.cedaro.com/
+ * Author:            Girard Media
+ * Author URI:        https://girardmedia.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.html
- * GitHub Plugin URI: cedaro/dequeue-jquery-migrate
+ * GitHub Plugin URI: girardmedia/jmigrate-fix
  */
 
 /**
@@ -27,10 +27,10 @@
  *
  * @param WP_Scripts $scripts WP_Scripts scripts object. Passed by reference.
  */
-function cedaro_dequeue_jquery_migrate( $scripts ) {
+function jmigrate_fix( $scripts ) {
 	if ( ! is_admin() && ! empty( $scripts->registered['jquery'] ) ) {
 		$jquery_dependencies = $scripts->registered['jquery']->deps;
 		$scripts->registered['jquery']->deps = array_diff( $jquery_dependencies, array( 'jquery-migrate' ) );
 	}
 }
-add_action( 'wp_default_scripts', 'cedaro_dequeue_jquery_migrate' );
+add_action( 'wp_default_scripts', 'jmigrate_fix' );
